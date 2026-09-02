bd1425a4924f810154282a6b22eb8d0c3e5e93f3
Yosselyn Guerrero <yosselynvillegas@gmail.com>
2026-09-01 18:11:39 -0600

    Calculadora basica de suma de dos numeros

--- MiPrimeraApp/MiPrimeraApp/Form1.Designer.cs
+++ MiPrimeraApp/MiPrimeraApp/Form1.Designer.cs
@@ -28,35 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSaludo = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnSaludo
+            // btnCalcular
             // 
-            this.btnSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSaludo.Location = new System.Drawing.Point(296, 173);
-            this.btnSaludo.Name = "btnSaludo";
-            this.btnSaludo.Size = new System.Drawing.Size(197, 90);
-            this.btnSaludo.TabIndex = 0;
-            this.btnSaludo.Text = "Saludo";
-            this.btnSaludo.UseVisualStyleBackColor = true;
-            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(35, 199);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 54);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(32, 49);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(67, 22);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "Num 1:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(113, 49);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(162, 22);
+            this.txtNum1.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(113, 104);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(162, 22);
+            this.txtNum2.TabIndex = 4;
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(32, 104);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(67, 22);
+            this.lblNum2.TabIndex = 3;
+            this.lblNum2.Text = "Num 2:";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(32, 153);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(116, 22);
+            this.lblRespuesta.TabIndex = 5;
+            this.lblRespuesta.Text = "Respuesta: ?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSaludo);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.btnCalcular);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculadora Basica";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblRespuesta;
     }
 }
 
--- MiPrimeraApp/MiPrimeraApp/Form1.cs
+++ MiPrimeraApp/MiPrimeraApp/Form1.cs
@@ -12,13 +12,23 @@ namespace MiPrimeraApp
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
+        
+        private void Form1_Load(object sender, EventArgs e) {
 
-        private void btnSaludo_Click(object sender, EventArgs e){
-            MessageBox.Show("Hola chicos de progra I, Bienvenidos");
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e) {
+            double num1, num2, resultado;
+            num1 = Double.Parse(txtNum1.Text);
+            num2 = Double.Parse(txtNum2.Text);
+
+            resultado = num1 + num2;
+
+            lblRespuesta.Text = "Respuesta: " + resultado.ToString();
         }
     }
 }
