ecaf62996843913e4711968b2de9b720ffa74a00
Yosselyn Guerrero <yosselynvillegas@gmail.com>
2026-09-23 09:29:48 -0600

    Creacion de la clase conexión y diseño del formulario

---
+++ ramaacademica/ramaacademica/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data; //Esta libreria es para trabajar con bases de datos
+using System.Data.SqlClient; //Esta libreria permite usar SQL Server
+
+
+namespace ramaacademica
+{
+    internal class Conexion {
+        //Definir los mienbros, atributos y metodos de la clase
+        public SqlConnection objConexion = new SqlConnection(); //conectarse a la base de datos
+        public SqlCommand objComando = new SqlCommand(); //Ejecutar consultas sql (insert, update, delete, select) en la BD
+        public SqlDataAdapter objDataAdapter = new SqlDataAdapter(); //Un puente entre la DB y la aplicación
+        DataSet objDs = new DataSet(); //Representa una copia en memoria de la arquitectura de la DB
+
+
+        public Conexion () { //Constructor e inialización de la clase
+            String cadenaCoenxion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\db_academica.mdf;Integrated Security=True";
+            objConexion.ConnectionString = cadenaCoenxion;
+            objConexion.Open(); //Abrir la DB
+        }
+         public DataSet obtenerDatos()
+        {
+            objDs .Clear();//Limpiar el dato
+            objComando.Connection = objConexion;
+
+            objDataAdapter.SelectCommand = objComando;
+            objComando.CommandText = "SELECT * FROM alumnos";
+            objDataAdapter.Fill(objDs, "alumnos");
+
+            return objDs;
+        }
+    }
+}
--- ramaacademica/ramaacademica/Form1.Designer.cs
+++ ramaacademica/ramaacademica/Form1.Designer.cs
@@ -28,21 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grbDatos = new System.Windows.Forms.GroupBox();
+            this.grbNavegacion = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblCodigoAlumno = new System.Windows.Forms.Label();
+            this.lblNombreAlumno = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grbDatos.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grbDatos
+            // 
+            this.grbDatos.Controls.Add(this.label2);
+            this.grbDatos.Controls.Add(this.textBox5);
+            this.grbDatos.Controls.Add(this.textBox4);
+            this.grbDatos.Controls.Add(this.label1);
+            this.grbDatos.Controls.Add(this.textBox3);
+            this.grbDatos.Controls.Add(this.lblDireccion);
+            this.grbDatos.Controls.Add(this.textBox2);
+            this.grbDatos.Controls.Add(this.textBox1);
+            this.grbDatos.Controls.Add(this.lblNombreAlumno);
+            this.grbDatos.Controls.Add(this.lblCodigoAlumno);
+            this.grbDatos.Location = new System.Drawing.Point(12, 12);
+            this.grbDatos.Name = "grbDatos";
+            this.grbDatos.Size = new System.Drawing.Size(765, 316);
+            this.grbDatos.TabIndex = 0;
+            this.grbDatos.TabStop = false;
+            this.grbDatos.Text = "DATOS";
+            // 
+            // grbNavegacion
+            // 
+            this.grbNavegacion.Location = new System.Drawing.Point(12, 334);
+            this.grbNavegacion.Name = "grbNavegacion";
+            this.grbNavegacion.Size = new System.Drawing.Size(363, 154);
+            this.grbNavegacion.TabIndex = 1;
+            this.grbNavegacion.TabStop = false;
+            this.grbNavegacion.Text = "Navegación";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(381, 334);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(363, 154);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Edicion";
+            // 
+            // lblCodigoAlumno
+            // 
+            this.lblCodigoAlumno.AutoSize = true;
+            this.lblCodigoAlumno.Location = new System.Drawing.Point(6, 24);
+            this.lblCodigoAlumno.Name = "lblCodigoAlumno";
+            this.lblCodigoAlumno.Size = new System.Drawing.Size(59, 16);
+            this.lblCodigoAlumno.TabIndex = 0;
+            this.lblCodigoAlumno.Text = "CODIGO";
+            // 
+            // lblNombreAlumno
+            // 
+            this.lblNombreAlumno.AutoSize = true;
+            this.lblNombreAlumno.Location = new System.Drawing.Point(6, 65);
+            this.lblNombreAlumno.Name = "lblNombreAlumno";
+            this.lblNombreAlumno.Size = new System.Drawing.Size(66, 16);
+            this.lblNombreAlumno.TabIndex = 1;
+            this.lblNombreAlumno.Text = "NOMBRE";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(3, 109);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(80, 16);
+            this.lblDireccion.TabIndex = 4;
+            this.lblDireccion.Text = "DIRECCION";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 106);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(360, 56);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TELEFONO";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 182);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(276, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(71, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(276, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "EMAIL";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.grbNavegacion);
+            this.Controls.Add(this.grbDatos);
             this.Name = "Form1";
-            this.Text = "Carpeta Academica";
+            this.Text = "Administracion de alumnos";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbDatos.ResumeLayout(false);
+            this.grbDatos.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbDatos;
+        private System.Windows.Forms.GroupBox grbNavegacion;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblNombreAlumno;
+        private System.Windows.Forms.Label lblCodigoAlumno;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
